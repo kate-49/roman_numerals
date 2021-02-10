7116ffbf2d00acae0cf3c549688ecbc8b81ab809
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-10 14:28:46 +0000

    passing test

--- roman_number_converter.rb
+++ roman_number_converter.rb
@@ -1,5 +1,19 @@
 class RomanToNum
-   def convert(letter)
-      return 0
+
+   def convert(letters)
+
+      valuehash = { "I" => 1 }
+
+      answer = 0
+      input = letters.split("")
+      
+      input.each do |x|
+         valuehash.each do |key, value|
+            if x == key
+               answer += value
+            end
+         end
+      end
+      return answer
    end
 end
