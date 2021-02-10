db48009c977e585971ac8af3bc0b0263f1571965
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-10 15:17:33 +0000

    passing tests for major letters

--- roman_number_converter.rb
+++ roman_number_converter.rb
@@ -2,7 +2,7 @@ class RomanToNum
    attr_accessor :hashvalues, :answer
 
    def initialize()
-      @hashvalues = { "I" => 1 }
+      @hashvalues = { "I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000 }
       @answer = 0
    end
 
--- spec/roman_number_converter_test.rb
+++ spec/roman_number_converter_test.rb
@@ -16,32 +16,39 @@ describe RomanToNum do
     end
 
     describe "can tell the values for all letters" do
-        converter = RomanToNum.new
+        
         it "returns 1 for I" do
+            converter = RomanToNum.new
             expect(converter.convert("I")).to eq(1)
         end
 
         it "returns 5 for V" do
+            converter = RomanToNum.new
             expect(converter.convert("V")).to eq(5)
         end
 
         it "returns 10 for X" do
+            converter = RomanToNum.new
             expect(converter.convert("X")).to eq(10)
         end
 
         it "returns 50 for L" do
+            converter = RomanToNum.new
             expect(converter.convert("L")).to eq(50)
         end
 
         it "returns 100 for C" do
+            converter = RomanToNum.new
             expect(converter.convert("C")).to eq(100)
         end
 
         it "returns 500 for D" do
+            converter = RomanToNum.new
             expect(converter.convert("D")).to eq(500)
         end
 
         it "returns 1000 for M" do
+            converter = RomanToNum.new
             expect(converter.convert("M")).to eq(1000)
         end
     end
