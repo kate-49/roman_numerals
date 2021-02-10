8c145bd6bfaece9f566fb180360074659ae63ee8
Kate Smith <kate.smith@digital.justie.gov.uk>
2021-02-10 15:15:39 +0000

    failing tests for all letter types

--- spec/roman_number_converter_test.rb
+++ spec/roman_number_converter_test.rb
@@ -14,4 +14,35 @@ describe RomanToNum do
         expect{converter.convert(true)}.to raise_error StandardError
         expect{converter.convert(false)}.to raise_error StandardError
     end
+
+    describe "can tell the values for all letters" do
+        converter = RomanToNum.new
+        it "returns 1 for I" do
+            expect(converter.convert("I")).to eq(1)
+        end
+
+        it "returns 5 for V" do
+            expect(converter.convert("V")).to eq(5)
+        end
+
+        it "returns 10 for X" do
+            expect(converter.convert("X")).to eq(10)
+        end
+
+        it "returns 50 for L" do
+            expect(converter.convert("L")).to eq(50)
+        end
+
+        it "returns 100 for C" do
+            expect(converter.convert("C")).to eq(100)
+        end
+
+        it "returns 500 for D" do
+            expect(converter.convert("D")).to eq(500)
+        end
+
+        it "returns 1000 for M" do
+            expect(converter.convert("M")).to eq(1000)
+        end
+    end
 end
